db24c6aa72fdc49079a9d3ee10bd9c5db335a78c
BlaineAtkins <blainegatkins@gmail.com>
2025-10-30 22:22:41 -0700

    Add files via upload

--- Firmware/Setup/EC2_New_Heart_Ingest.py
+++ Firmware/Setup/EC2_New_Heart_Ingest.py
@@ -3,8 +3,10 @@
 #1: Adds rows to AWS DynamoDB
 #2: Updates Mosquitto ACL with their usernames & passwords
 
+import os
 
 csv_file = 'NewHeartBatch.csv'
+mosquittoPWFile='/etc/mosquitto/LumasPWFile'
 devices = []
 
 with open(csv_file, 'r') as file:
@@ -12,20 +14,22 @@ with open(csv_file, 'r') as file:
 
 # Extract headers from the first line
 headers = lines[0].strip().split(',')
-
 # Process each subsequent line
 for line in lines[1:]:
     values = line.strip().split(',')
     device = dict(zip(headers, values))
     devices.append(device)
 
+print("Will now add rows to AWS DynamoDB")
 # Rows to add to DynamoDB:
 for device in devices:
-    print(device)
+    #print(device)
+    print("jk")
 
 
-print()
+print("Will now add entries to the Mosquitto Password file at "+mosquittoPWFile)
 #Things to add to Mosquitto ACL:
 for device in devices:
-    print(str(device['id'])+" - "+str(device['mqtt_password']))
-    
+    print(str(device['id'].replace(":",""))+" - "+str(device['mqtt_password']))
+
+    os.system(str("sudo mosquitto_passwd -b "+mosquittoPWFile+" "+str(device['id'].replace(":",""))+" "+str(device['mqtt_password'])))
